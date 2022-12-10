b6a48c20dd7bc7e09cfc3256c1ca26a8eea56fa9
dokim <dogdo5726@gmail.com>
2022-12-10 21:40:53 +0900

    [ADD] first commit

---
+++ src/main/java/com/dokim/younghan_spring_basic/YoungHanSpringBasicApplication.java
@@ -0,0 +1,13 @@
+package com.dokim.younghan_spring_basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoungHanSpringBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoungHanSpringBasicApplication.class, args);
+	}
+
+}
